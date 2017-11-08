0ef80b25b0cca66840dff0eb98612db99e31b615
Dieumi <oxeyo@hotmail.fr>
2017-11-08 16:02:20 -0500

    test mongo3

--- src/main/java/com/amigo/MongoConfig.java
+++ src/main/java/com/amigo/MongoConfig.java
@@ -3,6 +3,7 @@ package com.amigo;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
@@ -19,7 +20,8 @@ public class MongoConfig extends  AbstractMongoConfiguration {
     @Override
     @Bean
     public Mongo mongo() throws Exception {
-        return new MongoClient("mongodb://heroku_07nblqtz:ff08k99pomnicnmv69dtga0otb@ds249605.mlab.com:49605/");
+        MongoClientURI uri  = new MongoClientURI("mongodb://heroku_07nblqtz:ff08k99pomnicnmv69dtga0otb@ds249605.mlab.com:49605/heroku_07nblqtz");
+        return new MongoClient(uri);
     }
 
 }
